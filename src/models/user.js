8432782939ad06615f1c2abed5874f3be10808c9
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-16 09:28:40 +0200

    models pokemon user created

---
+++ src/models/user.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      createdAT: "created",
+      updatedAt: false,
+    }
+  );
+};
