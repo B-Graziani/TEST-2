ad24e4fb27ab97d82fa963c838d60a253312a6bc
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-16 11:18:43 +0200

    test connexion db production

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -5,14 +5,19 @@ const UserModel = require("../models/user");
 let sequelize;
 
 if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize("test_alone", "root", "root", {
-    port: 8889,
-    dialect: "mariadb",
-    dialectOptions: {
-      timezone: "Etc/GMT-2",
-    },
-    logging: true,
-  });
+  sequelize = new Sequelize(
+    "ifbptmf3mvx7acdd",
+    "nzuke8xnf7es5cyr",
+    "zxhgseq8z5p3gvpd",
+    {
+      host: "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      dialect: "mariadb",
+      dialectOptions: {
+        timezone: "Etc/GMT-2",
+      },
+      logging: true,
+    }
+  );
 } else {
   sequelize = new Sequelize("test_alone", "root", "root", {
     port: 8889,
