327c7d855427e4b74a9a4e4066ab8996f1bcab81
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-16 11:31:21 +0200

    test2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 //
 //DEBUT DE L APP
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 //
 //MIDDLEWARE
 app.use(bodyParser.json());
