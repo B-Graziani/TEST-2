788df6c3bcd9675dc2be2abe455f3d242e19cd91
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-16 11:10:03 +0200

    if bdd test heroku

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,14 +2,27 @@ const { Sequelize, DataTypes } = require("sequelize");
 const PokemonModel = require("../models/pokemon");
 const UserModel = require("../models/user");
 
-const sequelize = new Sequelize("test_alone", "root", "root", {
-  port: 8889,
-  dialect: "mariadb",
-  dialectOptions: {
-    timezone: "Etc/GMT-2",
-  },
-  logging: true,
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize("test_alone", "root", "root", {
+    port: 8889,
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: true,
+  });
+} else {
+  sequelize = new Sequelize("test_alone", "root", "root", {
+    port: 8889,
+    dialect: "mariadb",
+    dialectOptions: {
+      timezone: "Etc/GMT-2",
+    },
+    logging: true,
+  });
+}
 
 sequelize
   .authenticate()
