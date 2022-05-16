f736911232365340704ee33ab313a3418ccee405
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-16 17:00:35 +0200

    d

--- src/routes/pokemonRoutes/createPokemon.js
+++ src/routes/pokemonRoutes/createPokemon.js
@@ -1,22 +1,11 @@
 const { Pokemon } = require("../../db/sequelize");
-const { ValidationError, UniqueConstraintError } = require("sequelize");
+// const pokemon = require("../../models/pokemon");
 
 module.exports = (app) => {
-  app.post("/api/pokemon", (req, res) => {
-    Pokemon.create(req.body)
-      .then((pokemon) => {
-        const message = `Le pokémon ${req.body.name} a bien été crée.`;
-        res.json({ message, data: pokemon });
-      })
-      .catch((error) => {
-        if (error instanceof ValidationError) {
-          return res.status(400).json({ message: error.message, data: error });
-        }
-        if (error instanceof UniqueConstraintError) {
-          return res.statut(400).json({ message: error.message, data: error });
-        }
-        const message = "le pokemon n a pas pu etre creer , try again";
-        res.statut(500).json({ message, data, error });
-      });
+  app.post("/api/pokemons", (req, res) => {
+    Pokemon.create(req.body).then((pokemon) => {
+      const message = `le pokemon ${pokemon.name} a bien ete creeer`;
+      res.json({ message, data: pokemon });
+    });
   });
 };
