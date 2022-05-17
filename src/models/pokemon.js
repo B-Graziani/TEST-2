f6c706e8b9266ed411e18dd90c34861f0b63dec1
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-17 09:17:50 +0200

    bug resolu 2

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -64,6 +64,12 @@ module.exports = (sequelize, DataTypes) => {
       types: {
         type: DataTypes.STRING,
         allowNull: false,
+        get() {
+          return this.getDataValue("types").split(",");
+        },
+        set(types) {
+          this.setDataValue("types", types.join());
+        },
 
         validate: {
           isTypesValid(value) {
