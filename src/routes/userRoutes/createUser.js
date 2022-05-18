ab0b1f4747c4731a2ee1126ed9257511ad88856a
benoitgraziani <benoit.c.graziani@hotmail.fr>
2022-05-18 10:06:38 +0200

    user validate resolu

--- src/routes/userRoutes/createUser.js
+++ src/routes/userRoutes/createUser.js
@@ -1,4 +1,5 @@
 const { User } = require("../../db/sequelize");
+const { ValidationError, UniqueConstraintError } = require("sequelize");
 
 module.exports = (app) => {
   app.post("/api/user", (req, res) => {
@@ -7,8 +8,15 @@ module.exports = (app) => {
         const mess = `le user ${user.username} à bien ete creer !`;
         res.json({ mess, data: user });
       })
-      .catch(() => {
-        console.log("ca n a pas fonctionner !");
+      .catch((error) => {
+        if (error instanceof ValidationError) {
+          return res.status(400).json({ message: error.message, data: error });
+        }
+        if (error instanceof UniqueConstraintError) {
+          return res.statut(400).json({ message: error.message, data: error });
+        }
+        const message = "le user n a pas pu etre creer , try again";
+        res.statut(500).json({ message, data, error });
       });
   });
 };
